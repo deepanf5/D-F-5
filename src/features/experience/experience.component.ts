b172892223745c82ec9227997b3adaf60c29e9e7
deepan <deepanappu1999@gmail.com>
2024-05-26 19:40:52 +0530

    adding dynamic experience

--- src/features/experience/experience.component.ts
+++ src/features/experience/experience.component.ts
@@ -9,5 +9,18 @@ import { faExplosion } from '@fortawesome/free-solid-svg-icons';
 export class ExperienceComponent {
 
   explosion = faExplosion
+  yearsOfExperience:any;
+  roundedYearsOfExperience:any
+  remainingMonths:any
+
+  constructor(){
+    const startDate = new Date('2022-01-24');
+    const currentDate = new Date();
+    const difference = currentDate.getTime() - startDate.getTime();
+    this. yearsOfExperience = difference / (1000 * 3600 * 24 * 365);
+     this.roundedYearsOfExperience = Math.floor(this.yearsOfExperience);
+    this.remainingMonths = Math.floor((this.yearsOfExperience - this.roundedYearsOfExperience) * 12);
+  }
+  
 
 }
