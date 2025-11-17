d61fad6a37b7fcfa89e5e0387086e211297e50ae
deepan <deepanappu1999@gmail.com>
2025-11-17 19:25:47 +0530

    adding the new angular image path

--- src/features/skills/skills.component.ts
+++ src/features/skills/skills.component.ts
@@ -24,7 +24,7 @@ export class SkillsComponent {
         'https://upload.wikimedia.org/wikipedia/commons/thumb/archive/4/4c/20210506173340%21Typescript_logo_2020.svg/120px-Typescript_logo_2020.svg.png',
     },
     {
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Angular_gradient.png/1200px-Angular_gradient.png',
+      image: 'https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif',
     },
     {
       image:
