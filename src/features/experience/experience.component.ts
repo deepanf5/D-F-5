f223853d46d939512def9ca0a3359ef5908ee06f
deepan <deepanappu1999@gmail.com>
2026-04-27 21:24:05 +0530

    fixing the experience

--- src/features/experience/experience.component.ts
+++ src/features/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faExplosion } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,21 +6,22 @@ import { faExplosion } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit {
 
   explosion = faExplosion
-  yearsOfExperience:any;
-  roundedYearsOfExperience:any
-  remainingMonths:any
+  yearsOfExperience: any;
+  roundedYearsOfExperience: any
+  remainingMonths: any
 
-  constructor(){
+  constructor() {}
+  ngOnInit(): void {
     const startDate = new Date('2022-01-24');
-    const currentDate = new Date();
-    const difference = currentDate.getTime() - startDate.getTime();
-    this. yearsOfExperience = difference / (1000 * 3600 * 24 * 365);
-     this.roundedYearsOfExperience = Math.floor(this.yearsOfExperience);
-    this.remainingMonths = Math.floor((this.yearsOfExperience - this.roundedYearsOfExperience) * 12);
+    const endDate = new Date('2026-04-27');
+    const difference = endDate.getTime() - startDate.getTime();
+    const yearsOfExperience = difference / (1000 * 3600 * 24 * 365.25);
+    this.roundedYearsOfExperience = Math.floor(yearsOfExperience);
+    this.remainingMonths = Math.floor((yearsOfExperience - this.roundedYearsOfExperience) * 12);
   }
-  
+
 
 }
