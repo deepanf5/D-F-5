214773d17feec46dc0b23ab0a6421efc721200b8
deepan <deepanappu1999@gmail.com>
2026-04-28 10:46:41 +0530

    changing the skillposition

--- src/features/skills/skills.component.ts
+++ src/features/skills/skills.component.ts
@@ -8,27 +8,30 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class SkillsComponent {
   faCoffee = faCoffee;
-  skills = ['html', 'css', 'js', 'Ts', 'angular', 'react+ts'];
+  skills = ['angular', 'react', 'Ts', 'js','css','html',  ];
   skillsImage = [
-    {
-      image: 'https://www.svgrepo.com/show/349402/html5.svg',
-    },
-    {
-      image: 'https://www.svgrepo.com/show/349330/css3.svg',
-    },
-    {
-      image: 'https://www.svgrepo.com/show/452045/js.svg',
-    },
-    {
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/archive/4/4c/20210506173340%21Typescript_logo_2020.svg/120px-Typescript_logo_2020.svg.png',
-    },
-    {
+      {
       image: 'https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif',
     },
-    {
+     {
       image:
         'https://www.svgrepo.com/show/493719/react-javascript-js-framework-facebook.svg',
+    },
+  
+    {
+      image:
+        'https://www.svgrepo.com/show/349540/typescript.svg',
+    },
+  
+     {
+      image: 'https://www.svgrepo.com/show/452045/js.svg',
+    },
+   
+      {
+      image: 'https://www.svgrepo.com/show/349330/css3.svg',
+    },
+      {
+      image: 'https://www.svgrepo.com/show/349402/html5.svg',
     },
   ];
 }
